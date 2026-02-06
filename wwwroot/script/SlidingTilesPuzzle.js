3ac97fa9e6c9a46b95345afb592adb039b0fa203
Hershey Ann Estoya <hestoya@thurstonsoft.com>
2026-02-06 11:33:33 -0800

    Root Folder Setup

---
+++ wwwroot/script/SlidingTilesPuzzle.js
@@ -0,0 +1,71 @@
+﻿window.launchConfetti = function ()
+{
+    var duration = 5 * 1000;
+    var animationEnd = Date.now() + duration;
+    var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 999 };
+
+    var interval = setInterval(function () {
+        var timeLeft = animationEnd - Date.now();
+        if (timeLeft <= 0) return clearInterval(interval);
+
+        var particleCount = 50 * (timeLeft / duration);
+        confetti(Object.assign({}, defaults, { particleCount, origin: { x: Math.random(), y: Math.random() - 0.2 } }));
+    }, 250);
+}
+
+window.playWinSound = () => {
+    var audio = new Audio('assets/sounds/win.mp3');
+    audio.play().catch(e => console.error('Audio play error:', e));
+};
+
+window.playClickButtonSound = () => {
+    var audio = new Audio('assets/sounds/click.mp3');
+    audio.play().catch(e => console.error('Audio play error:', e));
+};
+
+let gameLoopAudio = null;
+
+window.playGameLoopSound = () => {
+    if (!gameLoopAudio) {
+        // Create and start the audio only if it hasn't been created already
+        gameLoopAudio = new Audio('assets/sounds/game-loop2.mp3');
+        gameLoopAudio.loop = true;  // Loop the sound for the duration of the game
+    }
+    gameLoopAudio.play().catch(e => console.error('Audio play error:', e));
+};
+
+// Function to stop (pause) the game loop sound
+window.stopGameLoopSound = () => {
+    if (gameLoopAudio) {
+        gameLoopAudio.pause();  // Pause the audio
+        gameLoopAudio.currentTime = 0;  // Optionally reset to the start
+    }
+};
+
+window.playTileMoveSound = () => {
+    var audio = new Audio('assets/sounds/swipe.mp3'); // Path to the sound for tile moves
+    audio.play().catch(e => console.error('Audio play error:', e));
+};
+
+window.playStartButtonSound = () => {
+    var audio = new Audio('assets/sounds/start.mp3'); // Path to the sound for tile moves
+    audio.play().catch(e => console.error('Audio play error:', e));
+};
+
+let homeLoopAudio = null;
+
+window.playHomeLoopSound = () => {
+    if (!homeLoopAudio) {
+        homeLoopAudio = new Audio('assets/sounds/game-loop1.mp3');
+        homeLoopAudio.loop = true;
+    }
+
+    homeLoopAudio.play().catch(e => console.error('Audio play error:', e));
+};
+
+window.stopHomeLoopSound = () => {
+    if (homeLoopAudio) {
+        homeLoopAudio.pause();
+        homeLoopAudio.currentTime = 0;
+    }
+};
